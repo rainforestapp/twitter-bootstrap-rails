830be394b86cf1a2759119959443452e9c8da012
Emanuel Evans <mail@emanuel.industries>
2017-09-28 15:23:41 -0700

    Change to a bogus version

--- lib/twitter/bootstrap/rails/version.rb
+++ lib/twitter/bootstrap/rails/version.rb
@@ -1,7 +1,9 @@
 module Twitter
   module Bootstrap
     module Rails
-      VERSION = "rainforest-dev"
+      # This is actually forked from 2.2.8, but we change the version to silence
+      # bundler-audit.
+      VERSION = "4.0.0.rainforest"
     end
   end
 end
